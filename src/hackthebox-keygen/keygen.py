07f69710578d4ee1da89d34b4cae4ca50c455c19
Michael Crump <mbcrump29@gmail.com>
2021-01-09 11:09:48 -0800

    bot push

---
+++ src/hackthebox-keygen/keygen.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# create a code to use with
+# hackthebox.eu
+
+import requests
+import base64
+
+url = 'https://www.hackthebox.eu/api/invite/generate'
+headers = {'content-type': 'application/json', 'user-agent': 'I bet you wish you knew'}
+x = requests.post(url, headers=headers)
+data = x.json()
+value = data['data']['code']
+
+base = base64.b64decode(value)
+print ("Your hackthebox invite code is : " + base.decode('utf-8'))
